f01c80be955d71c8dd986a0918da953ca6e5b0a1
ss-rowan <rowankortlandt@otsegops.org>
2024-01-04 20:27:38 -0500

    v2 nvim is submodule

---
+++ home-manager/home.nix
@@ -0,0 +1,153 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/home-manager):
+    # outputs.homeManagerModules.example
+
+    # Or modules exported from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModules.default
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      permittedInsecurePackages = [
+        "electron-25.9.0"
+      ];
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  home = {
+    username = "ss-rowan";
+    homeDirectory = "/home/ss-rowan";
+  };
+
+  home.file = {
+      ".config/nvim".source = ./nvim;
+      ".config/hypr".source = ./hyprland/hypr;
+      ".config/waybar".source = ./hyprland/waybar;
+      ".config/kitty".source = ./kitty;
+      ".config/starship.toml".source = ./starship.toml;
+  };
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+  };
+
+  gtk = {
+    enable = true;
+    theme.name = "adw-gtk3-dark";
+    theme.package = pkgs.adw-gtk3;
+    cursorTheme.package = pkgs.bibata-cursors;
+    cursorTheme.name = "Bibata-Modern-Classic";
+    iconTheme.package = pkgs.gnome.adwaita-icon-theme;
+    iconTheme.name = "Adwaita";
+  };
+
+  home.sessionVariables = {
+     EDITOR = "nvim";
+  };
+
+  services = {
+    dunst.enable = true;
+  };
+
+  programs = {
+    btop.enable = true;
+    waybar.enable = true;
+    neovim.enable = true;
+    starship.enable = true;
+    kitty.enable = true;
+    ripgrep.enable = true;
+
+    git = {
+      enable = true;
+      userName = "rkortlandt";
+      userEmail = "rowankortlandt@otsegops.org";
+    }; 
+
+    firefox.enable = true;
+    firefox.profiles.rowan = {
+      search = {
+        default = "Bing";
+        force = true;
+        engines."Nix-Packages" = {
+          urls = [{
+            template = "https://search.nixos.org/packages";
+            params = [
+              { name = "type"; value = "packages";}
+              { name = "query"; value = "{searchTerms}";}
+            ];
+          }];
+          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+          definedAliases = [ "@np" "@nix" "@packages" ];
+        };
+      };
+
+      extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
+        bitwarden
+        ublock-origin
+        sponsorblock
+      ];
+    }; 
+  };
+
+  home.packages = with pkgs; [
+    tofi 
+    swaybg
+    jetbrains.idea-ultimate
+    obsidian
+    grim
+    slurp
+    qemu
+    bridge-utils
+    discord
+    vivaldi
+    btop
+    jdk21
+  ];
+  
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.05";
+}
---
+++ nixos/configuration.nix
@@ -0,0 +1,174 @@
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other NixOS modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/nixos):
+    # outputs.nixosModules.example
+
+    # Or modules from other flakes (such as nixos-hardware):
+    # inputs.hardware.nixosModules.common-cpu-amd
+    # inputs.hardware.nixosModules.common-ssd
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./users.nix
+
+    # Import your generated (nixos-generate-config) hardware configuration
+    ./hardware-configuration.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
+  # This will add each flake input as a registry
+  # To make nix3 commands consistent with your flake
+  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+
+  # This will additionally add your inputs to the system's legacy channels
+  # Making legacy nix commands consistent as well, awesome!
+  nix.nixPath = ["/etc/nix/path"];
+  environment.etc =
+    lib.mapAttrs'
+    (name: value: {
+      name = "nix/path/${name}";
+      value.source = value.flake;
+    })
+    config.nix.registry;
+
+  nix.settings = {
+    # Enable flakes and new 'nix' command
+    experimental-features = "nix-command flakes";
+    # Deduplicate and optimize nix store
+    auto-optimise-store = true;
+  };
+  
+  #Networking
+  networking.networkmanager.enable = true;
+
+  # Hostname
+  networking.hostName = "rowan-nixos";
+
+  # Bootloader 
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  #TPM
+  security.tpm2 = {
+    enable = true;
+    pkcs11.enable = true;
+    tctiEnvironment.enable = true;
+  };
+
+#Audio
+
+  sound.enable = true;
+
+  hardware.pulseaudio = {
+    enable = true;
+    support32Bit = true;
+  };
+
+# Set your time zone.
+  time.timeZone = "America/Detroit";
+
+# Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  users.users = {
+    ss-rowan = {
+# Be sure to change it (using passwd) after rebooting!
+      initialPassword = "password";
+      isNormalUser = true;
+      description = "Rowan Kortlandt";
+        openssh.authorizedKeys.keys = [
+# TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+        ];
+      extraGroups = ["wheel" "networkmanager" "audio" "input" "libvirtd" "tss"];
+    };
+  };
+#Essential Packages
+  programs = {
+    thunar.enable = true;
+    virt-manager.enable = true;
+    hyprland.enable = true;
+  };
+
+#Auto login
+  services.greetd = {
+    enable = true;
+    settings = rec {
+      initial_session = {
+        command = "${pkgs.hyprland}/bin/Hyprland";
+        user = "ss-rowan";
+      };
+      default_session = initial_session;
+    };
+  };
+
+#Enable virtualization
+  virtualisation.libvirtd.enable = true;
+
+#Fonts
+  fonts.packages = with pkgs; [
+    font-awesome
+      (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
+  ];
+
+# This setups a SSH server. Very important if you're setting up a headless system.
+# Feel free to remove if you don't need it.
+#services.openssh = {
+#  enable = true;
+#  settings = {
+#    #Forbid root login through SSH.
+#    PermitRootLogin = "no";
+# Use keys only. Remove if you want to SSH using password (not recommended)
+#    PasswordAuthentication = false;
+#  };
+#};
+
+# https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "23.05";
+}
