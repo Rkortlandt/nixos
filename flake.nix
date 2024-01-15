d41494a7172703df32f26ab067fc36b77f07ba3f
rkortlandt <rowankortlandt@otsegops.org>
2024-01-14 23:00:20 -0500

    nix hardware

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     # at the same time. Here's an working example:
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
-
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     # Home manager
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -34,6 +34,7 @@
     self,
     nixpkgs,
     home-manager,
+    nixos-hardware,
     ...
   } @ inputs: let
     inherit (self) outputs;
@@ -74,6 +75,7 @@
         modules = [
           # > Our main nixos configuration file <
           ./nixos/configuration.nix
+	  nixos-hardware.framework-11th-gen-intel
         ];
       };
     };
