afee1e06d451bdaef8887fff6e61dc4be8faa50f
rkortlandt <rowankortlandt@otsegops.org>
2024-04-10 19:45:17 -0400

    fix ssh

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -148,6 +148,7 @@
       default_session = initial_session;
     };
   };
+  services.printing.enable = true;
   services.fwupd.enable = true;
   services.printing.enable = true;
 
@@ -181,15 +182,16 @@
 
 # This setups a SSH server. Very important if you're setting up a headless system.
 # Feel free to remove if you don't need it.
-#services.openssh = {
-#  enable = true;
-#  settings = {
-#    #Forbid root login through SSH.
-#    PermitRootLogin = "no";
+services.openssh = {
+  enable = true;
+  settings = {
+  #Forbid root login through SSH.
+  PermitRootLogin = "no";
 # Use keys only. Remove if you want to SSH using password (not recommended)
-#    PasswordAuthentication = false;
-#  };
-#};
+    PasswordAuthentication = false;
+    KbdInteractiveAuthentication = false;
+  };
+};
 
 # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
