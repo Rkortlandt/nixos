831ba2d3411359c0713a47cd1e18328869af1641
rkortlandt <rowankortlandt@otsegops.org>
2026-02-13 21:49:26 -0500

    audio update to bar

--- home-manager/agsbar/widget/Bar.tsx
+++ home-manager/agsbar/widget/Bar.tsx
@@ -10,7 +10,7 @@ import AstalNetwork from "gi://AstalNetwork"
 import AstalTray from "gi://AstalTray"
 import AstalMpris from "gi://AstalMpris"
 import AstalApps from "gi://AstalApps"
-import { For, With, createBinding, onCleanup } from "ags"
+import { For, With, createBinding, createState, onCleanup } from "ags"
 import { createPoll } from "ags/time"
 import { execAsync } from "ags/process"
 import { SpecialWorkspaces, Workspaces } from "./applets/Workspaces"
@@ -100,7 +100,10 @@ function Wireless() {
 }
 
 function AudioOutput() {
-  const { defaultSpeaker: speaker } = AstalWp.get_default()!
+  const [showAux, setShowAux] = createState(false);
+  const [showSinks, setShowSinks] = createState(false);
+  const { defaultSpeaker: speaker, audio: audio } = AstalWp.get_default()!
+  const speakerList = createBinding(audio, "speakers");
   function getVolumeIcon(): string {
     let volume = speaker.get_volume();
     if (speaker.get_mute()) {
@@ -117,23 +120,65 @@ function AudioOutput() {
   }
 
   return (
-    <button >
-      <box>
-        <Gtk.EventControllerScroll $={(self) => self.set_flags(Gtk.EventControllerScrollFlags.VERTICAL)}
-          onScroll={(_, __, dy) => {
-            speaker.volume = Math.max(0, Math.min(1, speaker.volume - (dy / 200)))
+    <box css="background: rgba(0, 0, 0, .7);">
+      <Gtk.EventControllerScroll $={(self) => self.set_flags(Gtk.EventControllerScrollFlags.VERTICAL)}
+        onScroll={(_, __, dy) => {
+          speaker.volume = Math.max(0, Math.min(1, speaker.volume - (dy / 200)))
 
-            return true;
-          }}
-        />
-        <label label={createBinding(speaker, "volume").as((volume) => (Math.floor(volume * 100)).toString() + "%")} />
-        <image
-          iconName={createBinding(speaker, "volumeIcon").as(() => getVolumeIcon())}
-          pixelSize={22}
-          css="padding-left: 3px"
-        />
-      </box>
-    </button>
+          return true;
+        }}
+      />
+      <Gtk.EventControllerMotion
+        onEnter={() => {
+          setShowAux(true);
+        }}
+        onLeave={() => {
+          setShowAux(false);
+        }}
+      />
+
+      <button onClicked={() => setShowSinks((s) => !s)}>
+        <box>
+          <label label={createBinding(speaker, "volume").as((volume) => (Math.floor(volume * 100)).toString() + "%")} />
+          <image
+            iconName={createBinding(speaker, "volumeIcon").as(() => getVolumeIcon())}
+            pixelSize={22}
+            css="padding-left: 3px;"
+          />
+        </box>
+      </button>
+      <revealer revealChild={showAux} transitionType={Gtk.RevealerTransitionType.SLIDE_RIGHT}>
+        <box css="padding-left: 4px;">
+          <revealer revealChild={showSinks.as((v) => !v)} transitionType={Gtk.RevealerTransitionType.SLIDE_RIGHT}>
+            <button class={"mic"} onClicked={() => { speaker.set_mute(!speaker.get_mute()) }}>
+              <image
+                iconName={"Speaker-Muted"}
+                pixelSize={22}
+                css="padding-left: 3px;"
+              />
+            </button>
+          </revealer>
+          <revealer revealChild={showSinks} transitionType={Gtk.RevealerTransitionType.SLIDE_RIGHT} >
+            <box spacing={4}>
+              <For each={speakerList}>
+                {(device) => (
+                  <button
+                    onClicked={() => {
+                      device.is_default = true
+                      console.log(`Switched to: ${device.description}`)
+                    }}
+
+                    class={createBinding(speaker, "name").as((name) => name == device.name ? "primary-bg" : "")}
+                  >
+                    <label label={device.name ? device.name : "Default"} />
+                  </button>
+                )}
+              </For>
+            </box>
+          </revealer>
+        </box >
+      </revealer >
+    </box >
   )
 }
 
@@ -179,7 +224,6 @@ function Backlight() {
           <image
             iconName={"Brightness"}
             pixelSize={22}
-            css=""
           />
           <label label={createBinding(bklight, "brightness").as(() => {
             var brightness = Math.ceil((bklight.brightness / bklight.maxBrightness) * 100).toString() + "%";
