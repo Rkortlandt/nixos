de388c8803347343d8d0a7ee791bf27548d4d44c
rkortlandt <rowankortlandt@otsegops.org>
2026-03-07 23:03:57 -0500

    home-manager: 20260307_230337

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,9 +7,22 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
-    # example = prev.example.overrideAttrs (oldAttrs: rec {
-    # ...
-    # });
+    freecad = final.unstable.freecad.overrideAttrs (oldAttrs: rec {
+      version = "1.1.0"; # Update this to the exact release tag if different
+
+      src = final.fetchFromGitHub {
+        owner = "FreeCAD";
+        repo = "FreeCAD";
+        tag = version;
+        # We use fakeHash first. Nix will fail the build and print the correct one.
+        hash = final.lib.fakeHash; 
+        fetchSubmodules = true;
+      };
+
+      # The 1.0.2 derivation uses patches that will almost certainly fail to apply 
+      # to the 1.1 source code. We clear them out to start fresh.
+      patches = [ ]; 
+    });
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
