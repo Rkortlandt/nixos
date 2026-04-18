cbb44793fb9339254576fd96578e50af0e32ad4e
rkortlandt <rowankortlandt@otsegops.org>
2026-04-18 17:09:41 -0400

    nixos-rebuild: 20260418_170852

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -87,16 +87,29 @@
     enable = true;
     libraries = with pkgs; [
       libsecret     # Fixes your current error
-      expat
+        expat
         glib
         gtk3
         webkitgtk_4_1
         xorg.libX11
         xorg.libXext
         xorg.libXxf86vm
+        xorg.libSM        
+        xorg.libICE        # Almost always required alongside libSM
+        nss                # Network Security Services (Huge culprit for web-based UIs)
+        nspr               # Pairs closely with NSS
+        alsa-lib           # Web engines constantly look for audio, even if silent
+        dbus               # Inter-process communication
+        cups               # Printing (A classic Chromium/CEF quirk)
+        xorg.libXcomposite # Standard X11 rendering extensions...
+        xorg.libXdamage    # ...
+        xorg.libXfixes     # ...
+        xorg.libXrandr     # ...
+        xorg.libxcb        # X C Binding
+        mesa               # OpenGL/Hardware acceleration
         libxkbcommon
         stdenv.cc.cc.lib # Often needed for standard C++ libraries
-    ];
+        ];
   };
   programs.dconf.enable = true;
   modules.hyprland.enable = lib.mkDefault true;
