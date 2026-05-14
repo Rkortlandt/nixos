ab70fc6b83676d94fed151b4d1f9fa492537be02
rkortlandt <rowankortlandt@otsegops.org>
2026-05-14 19:14:04 -0400

    home-manager: 20260514_191349

--- scripts/rhome.sh
+++ scripts/rhome.sh
@@ -9,7 +9,10 @@ find logs/ -name "*.log" -type f -mtime +30 -delete
 TEMP_LOG="logs/home_progress.log"
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 
-echo -e "\033[1;34m[STAGE 2/4]\033[0m Starting Home Manager Switch..."
+echo -e "\033[1;34m[STAGE 2/4]\033[0m Running Git add..."
+git add .
+
+echo -e "\033[1;34m[STAGE 3/4]\033[0m Starting Home Manager Switch..."
 systemd-inhibit --who="Home Manager" --why="Updating Config" --mode=block \
   home-manager switch --flake .#ss-rowan@rowan-nixos 2>&1 | tee "$TEMP_LOG"
 
@@ -17,7 +20,7 @@ if [ ${PIPESTATUS[0]} -eq 0 ]; then
     FINAL_LOG="logs/home_${TIMESTAMP}_SUCCESS.log"
     mv "$TEMP_LOG" "$FINAL_LOG"
     
-    echo -e "\033[1;32m[STAGE 3/4]\033[0m Switch Success. Committing to Git..."
+    echo -e "\033[1;32m[STAGE 4/4]\033[0m Switch Success. Committing to Git..."
     git add .
     git commit -m "home-manager: $TIMESTAMP"
     
