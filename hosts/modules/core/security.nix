b1815e0586a40540230966a09019e0268955efd8
rkortlandt <rowankortlandt@otsegops.org>
2026-05-19 10:21:06 -0400

    nixos-rebuild: 20260519_102025

--- hosts/modules/core/security.nix
+++ hosts/modules/core/security.nix
@@ -4,11 +4,17 @@
   security.polkit.enable = true;
   security.rtkit.enable = true;
   security.pam.services.login.enableKwallet = true;
+  security.pam.services.greetd.enableKwallet = true;
 
   #Force Chromium/Chrome/Brave to look for KWallet instead of guessing
   environment.sessionVariables = {
     CHROME_FLAGS = "--password-store=kwallet6"; 
   };
+
+  environment.systemPackages = with pkgs; [
+    kdePackages.kwallet
+    kdePackages.kwalletmanager # Optional graphical utility to manage it
+  ];
 
   # TPM
   security.tpm2 = {
